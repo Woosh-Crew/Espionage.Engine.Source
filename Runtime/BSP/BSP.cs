c10c27f9ffb8ae0182c7a308dc9eebe8144c0da3
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-14 13:37:16 +1100

    Add header reader to VTF

--- Runtime/BSP/BSP.cs
+++ Runtime/BSP/BSP.cs
@@ -27,7 +27,7 @@ namespace Espionage.Engine.Source
 
             Planes = Read( reader, Head.Lumps[1], 20, e => new Plane( e ) );
             TexDatas = Read( reader, Head.Lumps[2], 32, e => new TexData( e ) );
-            Vertices = Read( reader, Head.Lumps[3], 12, e => e.ReadSourceVec3( ) );
+            Vertices = Read( reader, Head.Lumps[3], 12, e => e.ReadSourceVec3() );
             Faces = Read( reader, Head.Lumps[7], 56, e => new Face( e ) );
             Edges = Read( reader, Head.Lumps[12], 4, e => new Edge( e ) );
             Cubemaps = Read( reader, Head.Lumps[42], 16, e => new Cubemap( e ) );
@@ -44,7 +44,7 @@ namespace Espionage.Engine.Source
             public Header( BinaryReader reader )
             {
                 Indent = reader.ReadBytes( 4 );
-                Version = reader.ReadInt32( );
+                Version = reader.ReadInt32();
 
                 // Read Lumps
                 Lumps = new Lump[64];
@@ -52,15 +52,15 @@ namespace Espionage.Engine.Source
                 {
                     var lump = Lumps[i];
 
-                    lump.Offset = reader.ReadInt32( );
-                    lump.Length = reader.ReadInt32( );
-                    lump.Version = reader.ReadInt32( );
+                    lump.Offset = reader.ReadInt32();
+                    lump.Length = reader.ReadInt32();
+                    lump.Version = reader.ReadInt32();
                     lump.Indent = reader.ReadBytes( 4 );
 
                     Lumps[i] = lump;
                 }
 
-                Revision = reader.ReadInt32( );
+                Revision = reader.ReadInt32();
             }
 
             public readonly byte[] Indent;
@@ -112,9 +112,9 @@ namespace Espionage.Engine.Source
         {
             public Plane( BinaryReader reader )
             {
-                Normal = reader.ReadSourceVec3( );
-                Distance = reader.ReadSingle( );
-                Type = reader.ReadInt32( );
+                Normal = reader.ReadSourceVec3();
+                Distance = reader.ReadSingle();
+                Type = reader.ReadInt32();
             }
 
             public readonly Vector3 Normal;
@@ -126,14 +126,14 @@ namespace Espionage.Engine.Source
         {
             public TexData( BinaryReader reader )
             {
-                Reflectivity = reader.ReadSourceVec3( );
-                NameID = reader.ReadInt32( );
+                Reflectivity = reader.ReadSourceVec3();
+                NameID = reader.ReadInt32();
 
-                Width = reader.ReadInt32( );
-                Height = reader.ReadInt32( );
+                Width = reader.ReadInt32();
+                Height = reader.ReadInt32();
 
-                ViewWidth = reader.ReadInt32( );
-                ViewHeight = reader.ReadInt32( );
+                ViewWidth = reader.ReadInt32();
+                ViewHeight = reader.ReadInt32();
             }
 
             public readonly Vector3 Reflectivity;
@@ -147,8 +147,8 @@ namespace Espionage.Engine.Source
             public Edge( BinaryReader reader )
             {
                 VertexIndices = new ushort[2];
-                VertexIndices[0] = reader.ReadUInt16( );
-                VertexIndices[1] = reader.ReadUInt16( );
+                VertexIndices[0] = reader.ReadUInt16();
+                VertexIndices[1] = reader.ReadUInt16();
             }
 
             public readonly ushort[] VertexIndices;
@@ -159,8 +159,8 @@ namespace Espionage.Engine.Source
             public SurfEdge( BinaryReader reader )
             {
                 VertexIndices = new ushort[2];
-                VertexIndices[0] = reader.ReadUInt16( );
-                VertexIndices[1] = reader.ReadUInt16( );
+                VertexIndices[0] = reader.ReadUInt16();
+                VertexIndices[1] = reader.ReadUInt16();
             }
 
             public readonly ushort[] VertexIndices;
@@ -173,13 +173,13 @@ namespace Espionage.Engine.Source
                 // We cant use ReadVec3 here, cause
                 // its actually 3 ints.
 
-                var x = reader.ReadInt32( );
-                var z = reader.ReadInt32( );
-                var y = reader.ReadInt32( );
+                var x = reader.ReadInt32();
+                var z = reader.ReadInt32();
+                var y = reader.ReadInt32();
 
                 Origin = new Vector3( x, y, z );
 
-                var res = reader.ReadInt32( );
+                var res = reader.ReadInt32();
                 Size = res == 0 ? 256 : res;
             }
 
@@ -191,30 +191,30 @@ namespace Espionage.Engine.Source
         {
             public Face( BinaryReader reader )
             {
-                PlaneNum = reader.ReadUInt16( );
-                Side = reader.ReadByte( );
-                OnNode = reader.ReadBoolean( );
-                FirstEdge = reader.ReadInt32( );
-                NumEdges = reader.ReadInt16( );
-                TexInfo = reader.ReadInt16( );
-                DisplacementInfo = reader.ReadInt16( );
-                SurfaceFogVolumeID = reader.ReadInt16( );
+                PlaneNum = reader.ReadUInt16();
+                Side = reader.ReadByte();
+                OnNode = reader.ReadBoolean();
+                FirstEdge = reader.ReadInt32();
+                NumEdges = reader.ReadInt16();
+                TexInfo = reader.ReadInt16();
+                DisplacementInfo = reader.ReadInt16();
+                SurfaceFogVolumeID = reader.ReadInt16();
                 Styles = reader.ReadBytes( 4 );
-                LightOffset = reader.ReadInt32( );
-                Area = reader.ReadSingle( );
+                LightOffset = reader.ReadInt32();
+                Area = reader.ReadSingle();
 
                 LightmapTextureMinsInLuxels = new int[2];
-                LightmapTextureMinsInLuxels[0] = reader.ReadInt32( );
-                LightmapTextureMinsInLuxels[1] = reader.ReadInt32( );
+                LightmapTextureMinsInLuxels[0] = reader.ReadInt32();
+                LightmapTextureMinsInLuxels[1] = reader.ReadInt32();
 
                 LightmapTextureSizeInLuxels = new int[2];
-                LightmapTextureSizeInLuxels[0] = reader.ReadInt32( );
-                LightmapTextureSizeInLuxels[1] = reader.ReadInt32( );
+                LightmapTextureSizeInLuxels[0] = reader.ReadInt32();
+                LightmapTextureSizeInLuxels[1] = reader.ReadInt32();
 
-                OriginalFace = reader.ReadInt32( );
-                NumPrims = reader.ReadUInt16( );
-                FirstPrimID = reader.ReadUInt16( );
-                SmoothingGroups = reader.ReadUInt32( );
+                OriginalFace = reader.ReadInt32();
+                NumPrims = reader.ReadUInt16();
+                FirstPrimID = reader.ReadUInt16();
+                SmoothingGroups = reader.ReadUInt32();
             }
 
             public readonly ushort PlaneNum;
--- Runtime/VTF/VTF.cs
+++ Runtime/VTF/VTF.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace Espionage.Engine.Source
@@ -7,6 +10,64 @@ namespace Espionage.Engine.Source
     /// <summary>Deserialized VTF Data</summary>
     public class VTF
     {
+        public Header Head { get; }
+        public FileInfo File { get; }
+
+        public VTF( FileInfo file )
+        {
+            File = file;
+
+            // Open Streams for reading the header
+            using var fileStream = File.Open( FileMode.Open, FileAccess.Read );
+            using var reader = new BinaryReader( fileStream );
+
+            Head = new Header( reader );
+        }
+
+        //
+        // Structs
+        //
+
+        public readonly struct Header
+        {
+            public string Format => Encoding.UTF8.GetString( Indent );
+
+
+            public Header( BinaryReader reader )
+            {
+                Indent = reader.ReadBytes( 4 );
+
+                Version = new uint[2];
+                Version[0] = reader.ReadUInt32();
+                Version[1] = reader.ReadUInt32();
+
+                Size = reader.ReadUInt32();
+
+                Width = reader.ReadUInt16();
+                Height = reader.ReadUInt16();
+
+                Flags = (Flags) reader.ReadUInt32();
+            }
+
+            public readonly byte[] Indent;
+            public readonly uint[] Version;
+            public readonly uint Size;
+
+            public readonly ushort Width;
+            public readonly ushort Height;
+
+            public readonly Flags Flags;
+        }
+
+        public readonly struct Entry
+        {
+            public Entry( BinaryReader reader ) { }
+        }
+
+        //
+        // Enums
+        //
+
         public enum Format
         {
             None = -1,
@@ -38,7 +99,8 @@ namespace Espionage.Engine.Source
             RGBA16161616,
             UVLX8888
         }
-        
+
+        [Flags]
         public enum Flags : uint
         {
             POINTSAMPLE = 0x00000001,
@@ -59,9 +121,9 @@ namespace Espionage.Engine.Source
             RENDERTARGET = 0x00008000,
             DEPTHRENDERTARGET = 0x00010000,
             NODEBUGOVERRIDE = 0x00020000,
-            SINGLECOPY	= 0x00040000,
+            SINGLECOPY = 0x00040000,
             PRE_SRGB = 0x00080000,
-        
+
             UNUSED_00100000 = 0x00100000,
             UNUSED_00200000 = 0x00200000,
             UNUSED_00400000 = 0x00400000,
@@ -72,14 +134,14 @@ namespace Espionage.Engine.Source
 
             CLAMPU = 0x02000000,
             VERTEXTEXTURE = 0x04000000,
-            SSBUMP = 0x08000000,			
+            SSBUMP = 0x08000000,
 
             UNUSED_10000000 = 0x10000000,
 
             BORDER = 0x20000000,
 
             UNUSED_40000000 = 0x40000000,
-            UNUSED_80000000 = 0x80000000,
+            UNUSED_80000000 = 0x80000000
         }
     }
 }

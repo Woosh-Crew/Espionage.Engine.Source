37e551c02d805920f937e234d64799791d920b3d
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-23 15:46:26 +1100

    Update FuncBrush so they can disable shadows

--- Runtime/BSP/Entities/FuncBrush.cs
+++ Runtime/BSP/Entities/FuncBrush.cs
@@ -1,18 +1,32 @@
 // ReSharper disable StringLiteralTypo
 
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Espionage.Engine.Source
 {
     [Library( "func_brush" ), Title( "Func Brush" ), Group( "Entities" ), Spawnable]
     public class FuncBrush : Behaviour, BSP.IBrushEntity
     {
+        [Property( "disablereceiveshadows" )]
+        public bool DisableReceiveShadows { get; set; }
+
+        [Property( "disableshadows" )]
+        public bool DisableCastingShadows { get; set; }
+
         public void OnRead( BSP.Entity ent, GameObject model )
         {
             // Set Parent
             model.transform.parent = transform;
-
             model.transform.localPosition = BSP.Vector.Parse( ent.KeyValues["origin"] );
+
+            var renderers = GetComponentsInChildren<MeshRenderer>();
+
+            foreach ( var meshRenderer in renderers )
+            {
+                meshRenderer.receiveShadows = !DisableReceiveShadows;
+                meshRenderer.shadowCastingMode = DisableCastingShadows ? ShadowCastingMode.Off : meshRenderer.shadowCastingMode;
+            }
         }
     }
 }

f13f8e2fbfbefbccc78e0396434e6760fa223338
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-23 12:59:41 +1100

    Add Angles, Color and Add Light Environment to Entity List

---
+++ Runtime/BSP/Entities/LightEnvironment.cs
@@ -0,0 +1,44 @@
+// ReSharper disable StringLiteralTypo
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Espionage.Engine.Source
+{
+    [Library( "light_environment" ), Title( "Sun Light" ), Group( "Entities" ), Spawnable]
+    public class LightEnvironment : Behaviour, BSP.IPointEntity
+    {
+        [Property( "pitch" )]
+        public float Pitch { get; set; }
+
+        [Property( "angles" )]
+        public BSP.Angles Angles { get; set; }
+        
+        [Property( "_light" )]
+        public BSP.Color Color { get; set; }
+        
+        [Property( "_ambient" )]
+        public BSP.Color Ambient { get; set; }
+
+        public void OnRead( BSP.Entity ent )
+        {
+            if ( _light != null )
+                return;
+            
+            // Create Directional Light
+            _light = gameObject.AddComponent<Light>();
+            _light.type = LightType.Directional;    
+            _light.shadows = LightShadows.Soft;
+            _light.color = Color;
+            _light.intensity = ( Color.Alpha / 100 ) / 4;
+            
+            RenderSettings.ambientLight = Ambient;
+            RenderSettings.sun = _light;
+            RenderSettings.ambientMode = AmbientMode.Flat;
+            
+            _light.transform.rotation = Quaternion.Euler( Angles.Pitch + -Pitch, Angles.Yaw + 120, Angles.Roll );
+        }
+
+        private static Light _light;
+    }
+}
---
+++ Runtime/BSP/Lumps/BSP.Angles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Espionage.Engine.Source
+{
+    public partial class BSP
+    {
+        public struct Angles
+        {
+            public float Pitch, Yaw, Roll;
+
+            public static Angles Parse( string value )
+            {
+                var split = value.Split( " " );
+
+                if ( split.Length < 3 )
+                    throw new InvalidCastException();
+
+                return new Angles()
+                {
+                    Pitch = float.Parse( split[0] ),
+                    Yaw = float.Parse( split[1] ),
+                    Roll = float.Parse( split[2] )
+                };
+            }
+        }
+    }
+}

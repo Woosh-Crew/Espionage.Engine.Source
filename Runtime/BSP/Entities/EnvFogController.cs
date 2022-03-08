7616b50d289b81e633ae8eef88a61bb467ed2521
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-08 12:12:04 +1100

    Add Info_Player_Start

--- Runtime/BSP/Entities/EnvFogController.cs
+++ Runtime/BSP/Entities/EnvFogController.cs
@@ -5,34 +5,34 @@ using UnityEngine.Rendering;
 
 namespace Espionage.Engine.Source
 {
-    [Library( "env_fog_controller" ), Title( "Fog Controller" )]
-    public class EnvFogController : SourceEntity, BSP.IPointEntity
-    {
-        // Colors
+	[Library( "env_fog_controller" ), Title( "Fog Controller" )]
+	public class EnvFogController : SourceEntity, BSP.IPointEntity
+	{
+		// Colors
 
-        [Property( "fogcolor" )]
-        public BSP.Color Color1 { get; set; }
+		[Property( "fogcolor" )]
+		public BSP.Color Color1 { get; set; }
 
-        // State & Distance
+		// State & Distance
 
-        [Property( "fogenable" )]
-        public bool Enabled { get; set; }
+		[Property( "fogenable" )]
+		public bool FogEnabled { get; set; }
 
-        [Property( "fogstart" )]
-        public float Start { get; set; }
+		[Property( "fogstart" )]
+		public float Start { get; set; }
 
-        [Property( "fogend" )]
-        public float End { get; set; }
+		[Property( "fogend" )]
+		public float End { get; set; }
 
-        public void OnRead( BSP.Entity ent )
-        {
-            // Apply Fog
+		public void OnRead( BSP.Entity ent )
+		{
+			// Apply Fog
 
-            RenderSettings.fog = Enabled;
-            RenderSettings.fogColor = Color1;
-            RenderSettings.fogMode = FogMode.Linear;
-            RenderSettings.fogStartDistance = Start / 100;
-            RenderSettings.fogEndDistance = End / 100;
-        }
-    }
+			RenderSettings.fog = FogEnabled;
+			RenderSettings.fogColor = Color1;
+			RenderSettings.fogMode = FogMode.Linear;
+			RenderSettings.fogStartDistance = Start / 100;
+			RenderSettings.fogEndDistance = End / 100;
+		}
+	}
 }
---
+++ Runtime/BSP/Entities/PlayerStart.cs
@@ -0,0 +1,5 @@
+﻿namespace Espionage.Engine.Source
+{
+	[Library( "info_player_start" ), Group( "Source Entities" ), Spawnable]
+	public class PlayerStart : SpawnPoint, BSP.IPointEntity { }
+}
